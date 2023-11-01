24737198fa01f20aa7b1bd4950f0078745bda430
Meghana <meghana.madhyastha@gmail.com>
2023-11-01 19:36:19 +0000

    Add server_provider with RPCs calling cpu intensive functions

--- server_provider.cpp
+++ server_provider.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <thallium.hpp>
 #include <thallium/serialization/stl/string.hpp>
+#include <unistd.h>
+#include <random>
+#include <chrono>
+
 
 namespace tl = thallium;
 
 class my_sum_provider : public tl::provider<my_sum_provider> {
 	private:
+
+		int cpu_intensive_task(){
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<> distr(0, 6);
+			int large_number = 5000;
+			int sum = 0;
+			for (int i=0; i<large_number*large_number; ++i){
+				sum += distr(gen);
+			}
+			return sum;
+		}
 		void prod(const tl::request& req, int x, int y) {
-			req.respond(x*y);
+			auto result = cpu_intensive_task();
+			req.respond(x*y + result);
 		}
 		int sum(int x, int y) {
-			return x+y;
+			auto result = cpu_intensive_task();
+			return x+y+result;
 		}
 
 		my_sum_provider(tl::engine& e, uint16_t provider_id=1)
@@ -19,7 +37,7 @@ class my_sum_provider : public tl::provider<my_sum_provider> {
 				define("prod", &my_sum_provider::prod );
 				get_engine().push_finalize_callback(this, [p=this]() { delete p; });
 			}
-	
+
 	public:
 		static my_sum_provider *create(tl::engine& e, uint16_t provider_id=1){
 			return new my_sum_provider(e, provider_id);
